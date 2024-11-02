46abccce0a15444e9aa02fd31e479dfb9317e0b8
Phearak <122800934+Samlothphearak@users.noreply.github.com>
2024-11-02 19:29:26 +0700

    implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,31 +1,38 @@
-import HeaderBox from '@/components/HeaderBox';
-import TotalBalanceBox from '@/components/TotalBalanceBox';
-
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-    const loggedIn = { firstName: 'Jennie'}; // Fixed missing quote and completed LastName
+  const loggedIn = { firstName: "Samloth", lastName: 'Phearak', email: 'samlothphearak@gmail.com'}; // Fixed missing quote and completed LastName
 
-    return (
-        <section className="home">
-            <div className="home-content">
-                <header className="home-header">
-                    <HeaderBox
-                        type="greeting"
-                        title="Welcome"
-                        subtext="Your digital banking experience"
-                        user={loggedIn?.firstName || 'Guest'} // Dynamically sets user prop
-                        subtext="Access and manage your account and transaction efficiently."
-                    />
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            subtext="Your digital banking experience"
+            user={loggedIn?.firstName || "Guest"} // Dynamically sets user prop
+            subtext="Access and manage your account and transaction efficiently."
+          />
 
-                    <TotalBalanceBox 
-                    accounts = {[]}
-                    totalBank={1}
-                    totalCurrentBalance= {10.89}
-                    />
-                </header>
-            </div>
-        </section>
-    );
+          <TotalBalanceBox
+            accounts={[]}
+            totalBank={1}
+            totalCurrentBalance={10.89}
+          />
+        </header>
+        RECENTLY TRANSACTIONS
+      </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance:1.89},{ currentBalance:1.78}]} // Make sure this is correctly typed
+      />
+    </section>
+  );
 };
 
 export default Home;
