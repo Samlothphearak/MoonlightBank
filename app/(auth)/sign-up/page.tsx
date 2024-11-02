bcc1bf05089e62186147f0e29c59816ce3d34e2c
Phearak <122800934+Samlothphearak@users.noreply.github.com>
2024-11-02 20:17:28 +0700

    fix login and signup

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import AuthForm from "@/components/AuthForm"; // Assuming AuthForm is a separate component
 
 const SignUp = () => {
-    return <div>SignUp</div>;
+    return <section className="flex-center size-full max-sm:px-6">
+        <AuthForm type="sign-up"/>
+    </section>;
 };
 
 export default SignUp;
+
--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -29,9 +29,6 @@ const MobileNav = ({ user }: MobileNavProps) => {
                     />
                 </SheetTrigger>
                 <SheetContent side="left" className="border-none bg-white">
-                    <SheetHeader>
-                        <SheetTitle className="text-black-1">Menu</SheetTitle>
-                    </SheetHeader>
                     <Link href="/" className="cursor-pointer flex items-center gap-1 px-4">
                         <Image
                             src="/icons/logo.svg"
